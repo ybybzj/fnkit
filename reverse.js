14cf20c38d089b93019b3cfd2fe23bd71ef57640
ybybzj <ybybzj@gmail.com>
2015-07-03 14:34:44 +0800

    update package.json

--- reverse.js
+++ reverse.js
@@ -9,20 +9,3 @@ var reverse = module.exports = function reverse(fn){
     return fn.apply(this, args.concat(rest));
   };
 };
-
-//test
-// if(require.main === module){
-//   var arr = [1,2,3];
-//   var curry = require('./curry');
-//   var __ = require('./placeholder');
-//   var fn = curry(2, reverse(function add(target,num){
-//     console.log('target', target);
-//     console.log('num', num);
-//     console.log('rest', slice(arguments, 2));
-//     return target + num;}))(3);
-//   console.log(fn(1, 0));
-//   console.log(fn(2, 1));
-//   console.log(arr.map(fn));
-//   // fn(1, 2);
-//   // reverse(fn)(2,1);
-// }
